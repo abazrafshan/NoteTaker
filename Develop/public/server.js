828443c129cf63664db0802ae8d04fdf84b14029
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-21 11:56:55 -0700

    get and post api routes operational

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -4,56 +4,36 @@ var fs = require("fs");
 var util = require("util");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 const readFileSync = util.promisify(fs.readFile);
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get("/api/notes", function(req,res){
+    fs.readFile("db.json", "utf-8", (err,data) => {
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post("/api/notes", (req,res) =>{
+    var newNote = req.body;
+    fs.appendFile("db.json", newNote, "utf8");
+    res.json(newNote);
+})
+
+// app.
 
 
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname,"notes.html"));
 });
 
-app.get("/", (req,res) => {
+app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname,"index.html"));
 });
-
-
-function readnotes(){
-    return readFileSync("db.json","utf8");
-}
-
-function getnotes(){
-    console.log("getnotes");
-    return readnotes().then(notes => {
-        var n = "";
-        console.log(notes);
-        try{
-            console.log("tryblock");
-        n = JSON.parse(notes);
-    }
-    catch(err){
-        console.log("error");
-        console.log(err);
-    }
-        return n;
-    })
-}
-app.get("/api/notes", (req, res) => {
-    return getnotes();
-    // fs.readFile("../db/db.json", (err, data)=>{
-    //     return data;
-    // });
-    // var test = ;
-    // console.log(test);
-    // return readFileSync("../db/db.json");
-});
-
-// app.post("api/notes", (req,res) => {
-//     var newNote = req.body;
-// });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
